828183a266b7ec33de534699135052dffa487219
dkang14 <davidkang@uchicago.edu>
2017-05-20 17:16:25 -0500

    added comments

--- TSNE_Clusters.py
+++ TSNE_Clusters.py
@@ -7,6 +7,7 @@ import seaborn
 
 
 def get_tickers(csvfile):
+    """Return tickers from csv file"""
     with open(csvfile, 'rb') as f:
         reader = csv.reader(f)
         data = [x[0] for x in reader][1:]
@@ -14,6 +15,7 @@ def get_tickers(csvfile):
 
 
 def quandl_adj_close(tickers):
+    """Retrieve Adj Close data from quandl"""
     quandl.ApiConfig.api_key = "ZF5iyY2nC7DHiWx49VhG"
     tickers = ["WIKI/" + x + ".11" for x in tickers]
     close_data = quandl.get(tickers, start_date="2015-01-01")
@@ -22,11 +24,13 @@ def quandl_adj_close(tickers):
 
 
 def corr_matrix(close_data):
+    """Return correlation matrix on absolute stock levels"""
     return close_data.corr()
 
 
 def tsne(corr, tickers):
-    model = TSNE(n_components=2, random_state=0)
+    """Use t-sne algorithm to reduce dimensionality to 2 and plot"""
+    model = TSNE(n_components=2)
     x = model.fit_transform(corr)
     plt.scatter(x[:, 0], x[:, 1], marker='o')
     for label, a, b in zip(tickers, x[:, 0], x[:, 1]):
@@ -35,6 +39,7 @@ def tsne(corr, tickers):
             xy=(a, b), xytext=(-20, 20),
             textcoords='offset points', ha='right', va='bottom',
             bbox=dict(boxstyle='round,pad=0.5', fc='white', alpha=0.5))
+    plt.title('T-SNE Clustering on the S&P 500 Correlation Matrix')
     plt.show()
 
 
